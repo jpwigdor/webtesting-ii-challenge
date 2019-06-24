553593bda27a8e56e900762b59c56367fca18d37
James Wigdor <jpwigdor@gmail.com>
2019-06-23 23:21:09 -0400

    Add Display COmponent

--- baseball/src/display/Display.js
+++ baseball/src/display/Display.js
@@ -1,1 +1,18 @@
-import React from "react";
+import React, { useState, useEffect, useReducer } from "react";
+
+function Display({ balls, strikes }) {
+  return (
+    <table>
+      <tr>
+        <th>Balls</th>
+        <th>Strikes</th>
+      </tr>
+      <tr>
+        <td>{balls}</td>
+        <td>{strikes}</td>
+      </tr>
+    </table>
+  );
+}
+
+export default Display;
